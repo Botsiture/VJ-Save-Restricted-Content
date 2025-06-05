5f078f4d5c4fae004495a8a13e627268cdd4d24e
Botsiture <bismillaha443@gmail.com>
2025-06-06 01:14:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "22634643"))
+API_ID = int(os.environ.get("API_ID", "10658015"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "c06eafa554174b9f10efb56757ed8592")
+API_HASH = os.environ.get("API_HASH", "a0087bca748f86698c53d291c9e5b3af")
 
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "7657218453"))
