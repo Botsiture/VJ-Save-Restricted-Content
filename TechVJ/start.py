ddd96b5cd11dc12d1abc6b5832b89e5dca5f3283
Botsiture <bismillaha443@gmail.com>
2026-06-01 21:30:51 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -61,10 +61,10 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("ＩＭ 𖣘︎ ＵＣＨＩＨＡ", url = "https://t.me/iMSASUKES7i")
+        InlineKeyboardButton("ＩＭ 𖣘︎ ＵＣＨＩＨＡ", url = "https://t.me/iMSASUKESi")
     ],[
-        InlineKeyboardButton('⌯ sᴜᴘᴘᴏʀᴛ ⌯', url='https://t.me/+kn9grAIIC5IyYzU9'),
-        InlineKeyboardButton('⌯ ᴄʜᴀɴɴᴇʟ ⌯', url='https://t.me/SANDVILLAGE')
+        InlineKeyboardButton('⌯ sᴜᴘᴘᴏʀᴛ ⌯', url='https://t.me/Anime_Group_hai'),
+        InlineKeyboardButton('⌯ ᴄʜᴀɴɴᴇʟ ⌯', url='https://t.me/SANDV_ILLAGE')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
